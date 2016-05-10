365870ee228d69042ab6d002665abc524971f94d
omarcarreon <oomarcarreon@gmail.com>
2016-05-10 17:54:55 -0500

    Se emplea el uso del raycast para destuir enemigos y hacer que te sigan

---
+++ Raycast_Tarea12_a01036074/Assets/Enemies.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+var goal: Transform;
+function Start () {
+      
+}
+
+function Update () {
+	var agent: NavMeshAgent = GetComponent.<NavMeshAgent>();
+  	agent.SetDestination(goal.transform.position);
+}
---
+++ Raycast_Tarea12_a01036074/Assets/Player.js
@@ -0,0 +1,17 @@
+﻿#pragma strict 
+function Start () { 
+}
+function Update () {
+	var hit : RaycastHit;
+	var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	Debug.DrawRay(ray.origin, ray.direction*10, Color.red);
+	if (Physics.Raycast(ray,hit)){
+		if (hit.transform.name == "Lumberjack1" || hit.transform.name == "Lumberjack2" || hit.transform.name == "Lumberjack3"){
+			Destroy(hit.transform.gameObject); 
+		}
+
+		print("I see a: "+ hit.transform.name+", and is"+hit.distance+"units apart");
+	} else{
+		print("I see nothing, nothing.");
+	}
+} 
