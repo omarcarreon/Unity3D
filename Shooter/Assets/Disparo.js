0f2d745c3fa49a6de9a03adc3b229b0d5ef9ba9c
omarcarreon <oomarcarreon@gmail.com>
2016-05-10 17:48:09 -0500

    Shooter simple

---
+++ Shooter/Assets/Disparo.js
@@ -0,0 +1,10 @@
+﻿var Bala: Rigidbody;
+var speed = 100;
+
+function Update() {
+	if(Input.GetButton("Fire1")){
+		clone = Instantiate(Bala, transform.position, transform.rotation);
+		clone.velocity = transform.TransformDirection(Vector3 (0,0,-speed));
+		Destroy(clone.gameObject, 1);
+	}
+}
