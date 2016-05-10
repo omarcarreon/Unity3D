4f0b0e4f2d8af07a5959b9afa746fca449065192
omarcarreon <oomarcarreon@gmail.com>
2016-05-10 18:35:39 -0500

    Actividad de zonas especiales

---
+++ ZonasEspeciales_A01036074/Assets/Gameplay.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Gameplay : MonoBehaviour {
+	public Text txtVida;
+	public Text txtScore;
+	public int vida;
+	public int score;
+	public Rigidbody esferaPrefab;
+
+	// Use this for initialization
+	void Start () {
+		vida = 100;
+		score = 0;
+		txtVida.text = "Vida: " + vida;
+		txtScore.text = "Score: " + score;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if(col.gameObject.name == "ZonaRecargaVida"){
+			vida += 50;
+			txtVida.text = "Vida: " + vida;
+		}
+
+		if(col.gameObject.name == "ZonaCreaEsferas"){
+			Instantiate(esferaPrefab, new Vector3(Random.Range (-4, 5), 4, Random.Range (-4, 5)), Quaternion.identity);
+			Instantiate(esferaPrefab, new Vector3(Random.Range (-4, 5), 4, Random.Range (-4, 5)), Quaternion.identity);
+			Instantiate(esferaPrefab, new Vector3(Random.Range (-4, 5), 4, Random.Range (-4, 5)), Quaternion.identity);
+			Instantiate(esferaPrefab, new Vector3(Random.Range (-4, 5), 4, Random.Range (-4, 5)), Quaternion.identity);
+		}
+
+
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if(collision.gameObject.name == "Sphere(Clone)"){
+			Destroy(collision.gameObject);
+			int opcion = Random.Range(1, 3);
+
+			if(opcion == 1){
+				score+= 100;
+				txtScore.text = "Score: " + score;
+			}
+			else{
+				vida -= 10;
+				txtVida.text = "Vida: " + vida;
+			}
+
+			if (score >= 1000) {
+				Application.LoadLevel("Ganar");
+			}
+			if (vida <= 0) {
+				Application.LoadLevel("Perder");
+			}
+
+		}
+	}
+}
---
+++ ZonasEspeciales_A01036074/Assets/SceneHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SceneHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadScene(string buttonName){
+        if (buttonName == "Salir")
+        {
+            Application.Quit();
+            
+        }
+        else
+        {
+            Application.LoadLevel(buttonName);
+        }
+
+	}
+}
+
