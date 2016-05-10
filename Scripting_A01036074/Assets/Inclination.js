f740cb91345cfdaf0fc5118da618ee18d323fd66
omarcarreon <oomarcarreon@gmail.com>
2016-05-10 18:51:34 -0500

    actividad scripting, destroy, inclination

---
+++ Scripting_A01036074/Assets/Inclination.js
@@ -0,0 +1,24 @@
+﻿import UnityEngine.UI;
+public var x = 0;
+public var y = 0;
+public var z = 0;
+
+function Update () {
+	if (Input.GetKey(KeyCode.UpArrow)){
+		x++;
+		this.transform.eulerAngles =  Vector3(x,0,z);
+	}
+	if (Input.GetKey (KeyCode.DownArrow)){
+		x--;
+		this.transform.eulerAngles =  Vector3(x,0,z);
+	}
+	if (Input.GetKey(KeyCode.LeftArrow)){
+		z++;
+		this.transform.eulerAngles =  Vector3(x,0,z);
+	}
+	if (Input.GetKey (KeyCode.RightArrow)){
+		z--;
+		this.transform.eulerAngles =  Vector3(x,0,z);
+	}
+		 
+}
